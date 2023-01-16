910e838c65d4f3a73662043e8235345d618d8ff6
ARSH <arshs287@gmail.com>
2023-01-16 14:07:28 +0530

    Update Cart

--- Music_Album/store.js
+++ Music_Album/store.js
@@ -5,20 +5,21 @@ for (var i = 0; i < removeCartItemButtons.length; i++) {
   button.addEventListener('click', function (event) {
     var buttonClicked = event.target;
     buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal();
   });
 }
 
-// function updateCartTotal() {
-//   var cartItemContainer = document.getElementsByClassName('cart-items')[0];
-//   var cartRows = cartItemContainer.getElementsByClassName('cart-row');
-//   for (var i = 0; i < cartRows.length; i++) {
-//     var cartRow = cartRows[i];
-//     var priceElement = cartRows.getElementsByClassName('cart-price')[0];
-//     var quantityElement = cartRow.getElementsByClassName(
-//       'cart-quantity-input'
-//     )[0];
-//     var price = parseFloat(priceElement.innerText.replace('$', ''));
-//     var quantity = quantityElement.value;
-//     console.log(price * quantity);
-//   }
-// }
+function updateCartTotal() {
+  var cartItemContainer = document.getElementsByClassName('cart-items')[0];
+  var cartRows = cartItemContainer.getElementsByClassName('cart-row');
+  for (var i = 0; i < cartRows.length; i++) {
+    var cartRow = cartRows[i];
+    var priceElement = cartRow.getElementsByClassName('cart-price')[0];
+    var quantityElement = cartRow.getElementsByClassName(
+      'cart-quantity-input'
+    )[0];
+    var price = parseFloat(priceElement.innerText.replace('$', ''));
+    var quantity = quantityElement.value;
+    console.log(price * quantity);
+  }
+}
