1a2e7ac020adf837541c62b3db71ac316db7d54e
ARSH <arshs287@gmail.com>
2023-11-01 13:36:30 +0530

    Made Color Changer Website

---
+++ Color-Changer/script.js
@@ -0,0 +1,22 @@
+// let index = 0;
+
+// function changeColors() {
+//   var colors = ['red', 'blue', 'green', 'yellow'];
+//   document.getElementsByTagName('body')[0].style.background = colors[index++];
+
+//   if (index > colors.length - 1) {
+//     index = 0;
+//   }
+// }
+
+let index = 0;
+
+function changeColors() {
+  var colors = ['red', 'gray', 'purple'];
+  document.getElementsByTagName('body')[0].style.backgroundColor =
+    colors[index++];
+
+  if (index > colors.length - 1) {
+    index = 0;
+  }
+}
